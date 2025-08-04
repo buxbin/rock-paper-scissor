f8b8d205b8bb72918c914586e87a9cc3ef4cb321
buxbin <110273995+buxbin@users.noreply.github.com>
2025-08-04 22:43:23 +0200

    added score variables, humanInput

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const getComputerChoice = function () {
+let computerScore = 0,
+  humanScore = 0;
+
+let humanInput = prompt("Choose rock, paper, scissor").toLowerCase();
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+function getComputerChoice() {
   let randomNum = Math.trunc(Math.random() * 3);
 
   switch (randomNum) {
@@ -11,10 +21,24 @@ const getComputerChoice = function () {
     case 2:
       return "scissor";
   }
-};
+}
 
-const getHumanChoice = function (choice) {
+function getHumanChoice(choice) {
   return choice;
-};
+}
 
-let humanInput = prompt("Choose rock, paper, scissor");
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock");
+  } else if (humanChoice === "rock" && computerChoice === "scissor") {
+    console.log("You win! Rock beats Scissor!");
+  } else if (humanChoice === "paper" && computerChoice == "rock") {
+    console.log("You win! Paper beats Rock!");
+  } else if (humanChoice === computerChoice) {
+    console.log(
+      `Its a draw! ${humanChoice
+        .charAt(0)
+        .toUpperCase()} can't beat ${computerChoice.charAt(0).toUpperCase()}`
+    );
+  }
+}
